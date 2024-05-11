b7cf0e831a85fbcad5f4a60ecc5347731f510939
vidhyassamuel <158567354+vidhyassamuel@users.noreply.github.com>
2024-05-11 19:50:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+
+const menu = document.querySelector(".menu");
+const menuList= document.querySelector("nav ul");
+menu.addEventListener('click',()=>{
+   menuList.classList.toggle(showmenu)
+  })
